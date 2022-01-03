31185b4e841e760cb32182c445582a7101c1aa3b
Deemee <deemee.9001@gmail.com>
2022-01-03 18:05:00 +0800

    Updates

---
+++ precache-manifest.c64d9d1e5d250a9266e2f2f8f27f8701.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3acb651c81fe64399b81",
+    "url": "/burger/static/css/main.6be5b4c1.chunk.css"
+  },
+  {
+    "revision": "3acb651c81fe64399b81",
+    "url": "/burger/static/js/main.3acb651c.chunk.js"
+  },
+  {
+    "revision": "335d0c99a13c44ab3250",
+    "url": "/burger/static/js/1.335d0c99.chunk.js"
+  },
+  {
+    "revision": "fd4f74309017cc0d11ea",
+    "url": "/burger/static/js/2.fd4f7430.chunk.js"
+  },
+  {
+    "revision": "b81fc2db2299c1069ccd",
+    "url": "/burger/static/js/runtime~main.b81fc2db.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  },
+  {
+    "revision": "04bdca957bf5c3af554e9ed7ec029711",
+    "url": "/burger/index.html"
+  }
+];
